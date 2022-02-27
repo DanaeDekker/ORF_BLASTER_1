dd5ec2579b4906222c50475f7e073c97bda23a91
mrijploeg <71006402+mrijploeg@users.noreply.github.com>
2022-02-27 09:34:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 from Bio import Entrez
 import re
+import time
 
 
 def read_file(file_name):
@@ -44,24 +45,26 @@ def blast(headers, seqs):
     """
     # Start the BLAST for each sequence in the list.
     for i in range(len(seqs)):
-        regex_match = re.search("ORF[1-9]{0,3}", headers[i])
-        orf = regex_match.group(0)
-        print(orf)
-        result_handle = NCBIWWW.qblast("blastx",
+        # regex_match = re.search("ORF[0-9]{1,3}", headers[i])
+        # orf = regex_match.group()
+        print(headers[i].replace(">", ""))
+        result_handle = NCBIWWW.qblast("blastp",
                                        "swissprot",
                                        seqs[i],
                                        expect=0.01,
                                        word_size=6,
                                        hitlist_size=10,
-                                       entrez_query=27300,
-                                       genetic_code=12)
+                                       entrez_query="txid4891[ORGN]")
 
         # Write the results in an xml-file.
-        with open("%s.xml" % orf, "w") as out_handle:
+        with open("{}.xml".format(headers[i].replace(">", "")), "w") \
+                as out_handle:
             out_handle.write(result_handle.read())
 
         # Close the result handle.
         result_handle.close()
+        print("Sleep...")
+        time.sleep(300.0)
 
 
 def parse_xml(header, seq):
@@ -75,9 +78,9 @@ def parse_xml(header, seq):
     rows = []
 
     # Open the xml-file and read it using Biopython's parse-function.
-    regex_match = re.search("ORF[1-9]{0,3}", header)
-    orf = regex_match.group(0)
-    result_handle = open("{}.xml".format(orf))
+    # regex_match = re.search("ORF[0-9]{1,3}", header)
+    # orf = regex_match.group(0)
+    result_handle = open("{}.xml".format(header.replace(">", "")))
     blast_record = NCBIXML.read(result_handle)
 
     # Add the alignment-data to a list. Each list contains ten lists
@@ -97,16 +100,18 @@ def parse_xml(header, seq):
             values.append(hsp.match)
 
         if values:
-            # Find the name of the species in the alignment-title.
-            species = re.search(r"(?<=\[).+?(?=\])", values[0])
-            scientific = str(species.group())
+            # Find the scientific name of the species in the title.
+            title = str(values[0]).split("[")
+            species = title[len(title) - 1]
+            scientific = species.replace("]", "")
 
             # Call the functions to get the neccesary data.
-            taxid = get_organism(scientific)
-            protein = get_protein(values[0], scientific)
+            taxid = get_taxid(scientific)
+            protein = get_protein(values[0])
 
             # Append the results.
-            row = str(str(orf) + "\t" + str(seq) + "\t" + str(taxid) +
+            row = str("augustus" + "\t" + str(header.replace(">", ""))
+                      + "\t" + str(seq) + "\t" + str(taxid) +
                       "\t" + str(scientific) + "\t" + str(values[1]) +
                       "\t" + str(protein) + "\t" + str(values[2]) +
                       "\t" + str(values[3]) + "\t" + str(values[4]) +
@@ -124,7 +129,7 @@ def parse_xml(header, seq):
     return rows
 
 
-def get_organism(scientific):
+def get_taxid(scientific):
     """ Get the taxid and lineage of an organism.
 
     :param scientific: string - scientific name of the organism
@@ -135,47 +140,26 @@ def get_organism(scientific):
     handle = Entrez.esearch(db="Taxonomy", term=scientific)
     record = Entrez.read(handle)
     taxid = record["IdList"][0]
-    handle = Entrez.efetch(db="Taxonomy", id=taxid, retmode="xml")
-    records = Entrez.read(handle)
-    taxonomy = records[0]["Lineage"]
-    lineage = taxonomy.split("; ")
-
-    # Split the scientific name into a list of genus and species.
-    scientific_list = scientific.split(" ")
-
-    # Sometimes, only the family or genus is in the name. We want a
-    # scientific name with
-    while len(scientific_list) < 2:
-        scientific_list.append("unknown")
-
-    # Create a string that contains the taxid, domain, genus en species,
-    # separated by tabs.
-    taxid_lineage = str(taxid + "\t" + lineage[1] + "\t" +
-                        scientific_list[0] + "\t" + scientific_list[1])
 
     # Return the string.
-    return taxid, taxid_lineage
+    return taxid
 
 
-def get_protein(title, scientific):
+def get_protein(title):
     """ Get the accession and the name of a protein.
 
     :param title: string - contains the name of the protein and the
     scientific name of the organism
-    :param scientific: string - scientific name of the organism
-    :return accession_protein: string - contains the accession code
-    and the name of the protein
+    :return protein: string - contains the name of the protein
     """
-    # Remove the scientific name from the title.
-    scientific_removed = title.replace("[" + scientific + "]", "")
-
-    # If there are multiple species in the title, a > is used, so split
-    # the string there so only one species is taken.
-    multispecies = scientific_removed.split(" >")
-
-    # In front of the protein name is the accession code. As we want to
-    # get them seperately, split on the |.
-    protein = multispecies[0].split("| ")
+    # Find the name of the protein.
+    full = re.search(r"Full=(.*)", title)
+    match = full.group(0)
+    if ";" in match:
+        protein = match.split(";")[0]
+    else:
+        protein = match.split("[")[0]
+    protein = protein.replace("Full=", "")
 
     # Return the string.
     return protein
@@ -183,22 +167,25 @@ def get_protein(title, scientific):
 
 if __name__ == '__main__':
     # Define the variables.
-    file_name = "orfs_nuc.fa"
+    file_name = "augustus_output.txt"
 
     # Read the txt-file with the headers, sequences and quality-codes.
     headers, seqs = read_file(file_name)
 
     # Use BLAST on the sequences.
-    blast(headers, seqs)
+    # blast(headers, seqs)
 
-    # Parse the xml-files. If results are found, append the lists to
-    # another list to combine them.
+    # Write a header for the tsv-file.
     with open("results.tsv", "w") as results_open:
-        results_open.write("orf\tseq\ttaxid\torganism\taccession\t"
-                           "protein\tlength\texpect\tscore\tidentities"
-                           "\tpositives\tgaps\tsubject\tquery\tmatch\n")
-        for i in range(len(headers)):
-            results = parse_xml(headers[i], seqs[i])
+        results_open.write("tool\tgene/orf\tseq\ttaxid\torganism"
+                           "\taccession\tprotein\tlength\texpect\tscore"
+                           "\tidentities\tpositives\tgaps\tsubject"
+                           "\tquery\tmatch\n")
+
+    # Parse the xml-files and append the results to a tsv-file.
+    with open("results.tsv", "a") as results_open:
+        for n in range(len(headers)):
+            results = parse_xml(headers[n], seqs[n])
             for result in results:
                 if result:
                     results_open.write(result + "\n")
